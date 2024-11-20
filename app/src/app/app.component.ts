76ff74f1ead16156f17ae62378a40ff25dbaa24e
William Lomar <lomarw99@gmail.com>
2024-11-20 11:46:12 -0300

    criando components de interface da aplicação

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatTabsModule } from '@angular/material/tabs';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatTabsModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'app';
+
 }
---
+++ app/src/app/cupcakes/cupcakes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cupcakes',
+  standalone: true,
+  imports: [],
+  templateUrl: './cupcakes.component.html',
+  styleUrl: './cupcakes.component.scss'
+})
+export class CupcakesComponent {
+
+}
